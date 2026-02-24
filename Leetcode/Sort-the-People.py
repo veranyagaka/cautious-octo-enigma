c5c544ebdb35ab2cfc84caee19fd9ad07c37a304
vera nyagaka <nyagakavera@gmail.com>
2026-02-24 22:57:28 +0300

    [2418] [Time Beats: 25.49%] [Memory Beats: 19.00%] - LeetPush

--- Leetcode/Sort-the-People.py
+++ Leetcode/Sort-the-People.py
@@ -1,8 +1,18 @@
 1class Solution:
 2    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-3        ## efficient sort
-4        
-5        together = list(zip(heights, names))
-6
-7        together = sorted(together, reverse=True)
-8        return [name for _, name in together]
+3        ## selection sort
+4        n = len(heights)
+5
+6        for i in range(n):
+7            min_index = i
+8
+9            # this is to find the minimum no
+10            for j in range(i+1, n): 
+11                if heights[j] > heights[min_index]: # cos its descenting
+12                    min_index = j
+13
+14            # swap
+15            heights[i], heights[min_index] = heights[min_index], heights[i]
+16            names[i], names[min_index] = names[min_index], names[i]
+17
+18        return names
