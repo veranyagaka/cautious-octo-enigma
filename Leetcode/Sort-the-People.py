6929607e66319f95f161ff3a06197db297e79a9f
vera nyagaka <nyagakavera@gmail.com>
2026-02-26 20:25:53 +0200

    [2418] [Time Beats: 28.82%] [Memory Beats: 7.87%] - LeetPush

--- Leetcode/Sort-the-People.py
+++ Leetcode/Sort-the-People.py
@@ -1,22 +1,18 @@
 1class Solution:
 2    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-3        ## insertion sort
-4        n = len(heights)
-5
-6        for i in range(1, n): # first element is always considered sorted
-7            key_height = heights[i]
-8            key_name = names[i]
-9
-10            j = i -1
-11
-12            while j >= 0 and heights[j] < key_height: # descending
-13                # move
-14                heights[j+1] = heights[j]
-15                names[j+1] = names[j]
-16                j -= 1
-17
-18            heights[j+1] = key_height
-19            names[j+1] = key_name
-20
-21
-22        return names
+3        ## counting sort
+4        max_height = max(heights)
+5        count = [""] * (max_height + 1)
+6        result = []
+7
+8        """index = height
+9        value = name
+10        """
+11        for i in range(len(heights)):
+12            count[heights[i]] = names[i]
+13
+14        for h in range(max_height, -1, -1): # descending order
+15            if count[h] != "":
+16                result.append(count[h])
+17        
+18        return result
