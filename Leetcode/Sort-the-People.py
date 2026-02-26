3a515f8f1835b27916a5bc81dc8338d3088a81b0
vera nyagaka <nyagakavera@gmail.com>
2026-02-26 19:54:59 +0200

    [2418] [Time Beats: 21.15%] [Memory Beats: 74.91%] - LeetPush

--- Leetcode/Sort-the-People.py
+++ Leetcode/Sort-the-People.py
@@ -1,18 +1,22 @@
 1class Solution:
 2    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-3        ## selection sort
+3        ## insertion sort
 4        n = len(heights)
 5
-6        for i in range(n):
-7            min_index = i
-8
-9            # this is to find the minimum no
-10            for j in range(i+1, n): 
-11                if heights[j] > heights[min_index]: # cos its descenting
-12                    min_index = j
-13
-14            # swap
-15            heights[i], heights[min_index] = heights[min_index], heights[i]
-16            names[i], names[min_index] = names[min_index], names[i]
+6        for i in range(1, n): # first element is always considered sorted
+7            key_height = heights[i]
+8            key_name = names[i]
+9
+10            j = i -1
+11
+12            while j >= 0 and heights[j] < key_height: # descending
+13                # move
+14                heights[j+1] = heights[j]
+15                names[j+1] = names[j]
+16                j -= 1
 17
-18        return names
+18            heights[j+1] = key_height
+19            names[j+1] = key_name
+20
+21
+22        return names
