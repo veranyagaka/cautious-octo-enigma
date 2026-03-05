89fd9bd49239641e56ec4067cfd20b746aae993d
vera nyagaka <nyagakavera@gmail.com>
2026-03-05 13:12:51 +0200

    [1758] [Time Beats: 88.05%] [Memory Beats: 83.47%] - LeetPush

--- Leetcode/Minimum-Changes-To-Make-Alternating-Binary-String.py
+++ Leetcode/Minimum-Changes-To-Make-Alternating-Binary-String.py
@@ -1,32 +1,21 @@
 1class Solution:
 2    def minOperations(self, s: str) -> int:
 3
-4        pattA, pattB = 0, 0
-5        n = len(s)
-6        str1 = [0] * n
-7        for x in range(0, n - 1, 2):
-8            str1[x] = 0
-9            str1[x+1] = 1
-10        
-11        str2 = [1] * n
-12        for x in range(0, n - 1, 2):
-13            str2[x] = 1
-14            str2[x+1] = 0
-15
-16        for i in range(n):
-17        
-18            if int(s[i]) != str1[i]:
-19                pattA += 1
-20
-21        for i in range(n):
-22            if int(s[i]) != str2[i]:
-23                pattB += 1
-24        # print(str1)
-25        # print(str2)
-26        
-27        ans = min(pattA, pattB)
-28        return ans
-29
-30        """
-31        two patterns A and B; A: starts with 0 
-32        and B starts with 1"""
+4        patA, patB = 0, 0
+5        expectedA, expectedB = "0", "1"
+6
+7        for c in s:
+8            if c != expectedA:
+9                patA += 1
+10
+11            if c != expectedB:
+12                patB += 1
+13
+14            expectedA = "1" if expectedA == "0" else "0"
+15            expectedB = "0" if expectedB == "1" else "1"
+16
+17        return min(patA, patB)
+18
+19        """
+20        two patterns A and B; A: starts with 0 
+21        and B starts with 1"""
