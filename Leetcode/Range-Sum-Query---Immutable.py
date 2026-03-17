14338af87012d2b7d039a349caf78ddbb08d112c
vera nyagaka <nyagakavera@gmail.com>
2026-03-17 19:53:11 +0200

    [303] [Time Beats: 62.02%] [Memory Beats: 72.92%] - LeetPush

--- Leetcode/Range-Sum-Query---Immutable.py
+++ Leetcode/Range-Sum-Query---Immutable.py
@@ -1,21 +1,19 @@
 1class NumArray:
 2
 3    def __init__(self, nums: List[int]):
-4        self.nums = nums
-5        
-6
-7    def sumRange(self, left: int, right: int) -> int:
-8        running_sum = 0
-9        while left <= right:
-10            running_sum += self.nums[left]
-11            left += 1
-12
+4        self.prefix = [0] * (len(nums)+1)
+5
+6        for i  in range(len(nums)):
+7            self.prefix[i+1] = self.prefix[i] + nums[i]
+8
+9        
+10
+11    def sumRange(self, left: int, right: int) -> int:
+12        return self.prefix[right+1] - self.prefix[left]
 13
-14        return running_sum
+14        
 15
-16        
-17
-18
-19# Your NumArray object will be instantiated and called as such:
-20# obj = NumArray(nums)
-21# param_1 = obj.sumRange(left,right)
+16
+17# Your NumArray object will be instantiated and called as such:
+18# obj = NumArray(nums)
+19# param_1 = obj.sumRange(left,right)
