4acd9f20b8d76aaa2a35bcd01c5cf9c7ada65281
vera nyagaka <nyagakavera@gmail.com>
2026-03-20 16:58:59 +0200

    [881] [Time Beats: 47.29%] [Memory Beats: 68.73%] - LeetPush

--- Leetcode/Boats-to-Save-People.py
+++ Leetcode/Boats-to-Save-People.py
@@ -3,25 +3,19 @@
 3        people.sort()
 4        
 5        boats = 0
-6        running_sum = 0
-7        n = len(people)
-8        l = 0
-9        r = n -1
-10        
-11        while l <= r and r < n:
-12
-13            if people[l] + people[r] > limit:
-14                boats += 1
-15                r -= 1
-16
-17            elif people[l] + people[r] <= limit:
-18                boats += 1
-19                r -= 1
-20                l +=1
-21            
-22            else:
-23                boats += 1
-24                l += 1
-25
-26
-27        return boats
+6        n = len(people)
+7        l = 0
+8        r = n -1
+9        
+10        while l <= r and r < n:
+11
+12            if people[l] + people[r] > limit:
+13                boats += 1
+14                r -= 1
+15
+16            else: #people[l] + people[r] <= limit
+17                boats += 1
+18                r -= 1
+19                l +=1
+20            
+21        return boats
