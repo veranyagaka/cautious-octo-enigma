1597a990f2f13b782dfd98702976ac020c2b876a
vera nyagaka <nyagakavera@gmail.com>
2026-03-20 17:31:15 +0200

    [633] [Time Beats: 70.20%] [Memory Beats: 82.05%] - LeetPush

--- Leetcode/Sum-of-Square-Numbers.py
+++ Leetcode/Sum-of-Square-Numbers.py
@@ -4,16 +4,18 @@
 4        ## search space = n ** .5 + 1
 5
 6        a = 0
-7        b = int(c ** .5) + 1
+7        b = int(c ** .5) + 1 ## make sure its an integer
 8
 9        while a <= b:
-10            if c == a * a + b * b:
-11                return True
-12
-13            elif a * a + b * b > c:
-14                b -= 1
-15
-16            else:
-17                a += 1
-18
-19        return False
+10            curr = a * a + b * b
+11            
+12            if c == curr:
+13                return True
+14
+15            elif curr > c:
+16                b -= 1
+17
+18            else:
+19                a += 1
+20
+21        return False
