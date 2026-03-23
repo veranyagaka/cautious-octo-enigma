1401578e0a3a1c544ab14d936779540222806e05
vera nyagaka <nyagakavera@gmail.com>
2026-03-23 07:45:26 +0200

    [2574] [Time Beats: 71.82%] [Memory Beats: 74.07%] - LeetPush

--- Leetcode/Left-and-Right-Sum-Differences.py
+++ Leetcode/Left-and-Right-Sum-Differences.py
@@ -1,22 +1,14 @@
 1class Solution:
 2    def leftRightDifference(self, nums: List[int]) -> List[int]:
-3        leftSum = [0]
-4        rightSum = [0]
-5        l_run_sum, r_run_sum = 0, 0
-6        n = len(nums)
-7        ans = [0] * n
-8
-9        for i in range(n-1):
-10            l_run_sum += nums[i]
-11            leftSum.append(l_run_sum)
+3        
+4        answer = []
+5        total = sum(nums)
+6        leftSum = 0
+7
+8        for x in nums:
+9            total -= x # right
+10            answer.append(abs(total - leftSum))
+11            leftSum += x
 12
-13        for i in range(n-1, 0, -1):
-14            r_run_sum += nums[i]
-15            rightSum.append(r_run_sum)
-16
-17        rightSum = rightSum[::-1]
-18
-19        for i in range(n):
-20            ans[i] = abs(leftSum[i] - rightSum[i])
-21
-22        return ans
+13
+14        return answer
