ed81a25437b005708b6a15430e3637b4bd38325e
vera nyagaka <nyagakavera@gmail.com>
2026-04-07 21:59:26 +0300

    [20] [Time Beats: 100.00%] [Memory Beats: 77.84%] - LeetPush

--- Leetcode/Valid-Parentheses.py
+++ Leetcode/Valid-Parentheses.py
@@ -1,15 +1,26 @@
 1class Solution:
 2    def isValid(self, s: str) -> bool:
 3        stack = []
-4        hashmap = {')': '(', ']':'[', '}': '{'}
+4        hashmap = {"{": "}", "[": "]", "(": ")"}
 5
 6        for ch in s:
 7            if ch in hashmap:
-8                top_element = stack.pop() if stack else "#"
-9
-10                if top_element != hashmap[ch]:
+8                stack.append(ch)
+9            else:
+10                if len(stack) == 0:
 11                    return False
-12            else:
-13                stack.append(ch)
-14        
-15        return not stack
+12
+13                pop_element = stack.pop()
+14                if hashmap[pop_element] != ch:
+15                    return False
+16
+17        return not stack
+18        
+19        """
+20        a2sv
+21        approach if
+22        we come accross an opening we add it to the stack
+23        else we pop and check 
+24        edge cases; stack is empty or not
+25        there must be closed if not false 
+26        """
