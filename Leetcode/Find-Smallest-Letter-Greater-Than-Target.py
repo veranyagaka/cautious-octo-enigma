a5bd95119ef72ef744b101db4ae197f56111c8bb
vera nyagaka <nyagakavera@gmail.com>
2026-04-25 16:54:01 +0300

    [744] [Time Beats: 100.00%] [Memory Beats: 54.99%] - LeetPush

--- Leetcode/Find-Smallest-Letter-Greater-Than-Target.py
+++ Leetcode/Find-Smallest-Letter-Greater-Than-Target.py
@@ -1,15 +1,28 @@
 1class Solution:
 2    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
 3        ## convert them to numbers
-4
-5        nums = [ord(x) for x in letters]
-6        target_num = ord(target)
-7
-8        for num in nums:
-9            if num > target_num:
-10                return chr(num)
-11
-12
-13        ##print(nums)
-14        return chr(nums[0])
-15
+4        ## letters is sorted
+5        ## supposed to use binary search
+6
+7        nums = [ord(x) for x in letters]
+8        target_num = ord(target)
+9        low, high = 0, len(letters) - 1
+10
+11        while low <= high:
+12            mid = (low + high) // 2
+13
+14            if nums[mid] > target_num:
+15                ## how low can we go
+16                high = mid - 1
+17
+18            else:
+19                low = mid + 1
+20
+21        ## else we are returning the first chatacter in letters
+22
+23
+24        if low == len(letters):
+25            return chr(nums[0])
+26        
+27        return chr(nums[low])
+28
