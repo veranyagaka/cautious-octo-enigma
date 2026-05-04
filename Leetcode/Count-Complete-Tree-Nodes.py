62ac63236245b7913487630a9abe63cce15989da
vera nyagaka <nyagakavera@gmail.com>
2026-05-04 10:55:38 +0300

    [222] [Time Beats: 100.00%] [Memory Beats: 48.56%] - LeetPush

--- Leetcode/Count-Complete-Tree-Nodes.py
+++ Leetcode/Count-Complete-Tree-Nodes.py
@@ -7,11 +7,42 @@
 7class Solution:
 8    def countNodes(self, root: Optional[TreeNode]) -> int:
 9        ## less than o(n) TC
-10
-11        ## brute force first
-12        if not root: return 0
+10        ## compute the height of left and right
+11        ## if equal = formula
+12        ## else just traverse on the side that aint perfect
 13
-14        left = self.countNodes(root.left)
-15        right = self.countNodes(root.right)
-16
-17        return 1 + left + right 
+14        if not root: return 0
+15
+16        def get_left_height(node):
+17            height = 0
+18            while node:
+19                height += 1
+20                node = node.left
+21
+22            return height
+23
+24        def get_right_height(node):
+25            height = 0
+26            while node:
+27                height += 1
+28                node = node.right
+29
+30            return height
+31
+32        left_h = get_left_height(root)
+33        right_h = get_right_height(root)
+34
+35        if left_h == right_h:
+36            return (2 ** left_h)- 1
+37
+38        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+39
+40
+41
+42            
+43
+44
+45        # left = self.countNodes(root.left)
+46        # right = self.countNodes(root.right)
+47
+48        # return 1 + left + right 
