9e2ab60ef9c6945696e02ed0e7b81a6f27c4b422
vera nyagaka <nyagakavera@gmail.com>
2026-05-05 13:38:29 +0300

    [81] [Time Beats: 100.00%] [Memory Beats: 69.84%] - LeetPush

---
+++ Leetcode/Search-in-Rotated-Sorted-Array-II.py
@@ -0,0 +1,10 @@
+1class Solution:
+2    def search(self, nums: List[int], target: int) -> bool:
+3        ## supposed to use binary search
+4
+5        ## brute force first
+6
+7        if target in nums: 
+8            return True
+9
+10        return False
