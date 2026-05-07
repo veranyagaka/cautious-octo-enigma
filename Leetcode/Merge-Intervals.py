c48e09edfaec8febaabee9a0da36275195562c58
vera nyagaka <nyagakavera@gmail.com>
2026-05-07 19:58:24 +0300

    [56] [Time Beats: 72.13%] [Memory Beats: 99.70%] - LeetPush

--- Leetcode/Merge-Intervals.py
+++ Leetcode/Merge-Intervals.py
@@ -1,15 +1,18 @@
 1class Solution:
 2    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-3        intervals.sort(key = lambda x: x[0])
-4        merged = [intervals[0]]
-5
-6        for curr in intervals[1:]:
-7            prev = merged[-1] # get the last thing merged: not copy a reference to the list
+3        
+4        ## first sort by the first no
+5        intervals = sorted(intervals, key=lambda x: x[0])
+6
+7        res = [intervals[0]]
 8
-9            if curr[0] <= prev[1]: # overlap!
-10                prev[1] = max(curr[1], prev[1])
-11            else:
-12                merged.append(curr)
-13
+9        for i in intervals[1:]:
+10            prev = res[-1]
+11
+12            if prev[1] >= i[0]: # we need to merge
+13                prev[1] = max(i[1], prev[1])
 14
-15        return merged
+15            else:
+16                res.append(i)
+17
+18        return res
