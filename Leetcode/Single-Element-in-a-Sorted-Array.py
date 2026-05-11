31cf00d8415b4a7b214fcc5f4dfd6e068bc48e56
vera nyagaka <nyagakavera@gmail.com>
2026-05-11 08:07:44 +0300

    [540] [Time Beats: 100.00%] [Memory Beats: 29.42%] - LeetPush

--- Leetcode/Single-Element-in-a-Sorted-Array.py
+++ Leetcode/Single-Element-in-a-Sorted-Array.py
@@ -1,51 +1,50 @@
 1class Solution:
 2    def singleNonDuplicate(self, nums: List[int]) -> int:
 3
-4        ##edge case
-5        if len(nums) == 1:
-6            return nums[0]
-7        
-8        low = 0
-9        high = len(nums) - 1
-10
-11        while low < high:
-12            mid = (low + high ) // 2
+4        l = 0
+5        r = len(nums) - 1
+6
+7        if len(nums) == 1:
+8            return nums[0]
+9
+10        while nums[l] == nums[l + 1] and nums[r - 1] == nums[r]:
+11            l += 2
+12            r -= 2
 13
-14            if mid % 2 == 0:
-15
-16                if nums[mid] == nums[mid + 1]:
-17                    ## the single element is further right
-18                    low = mid + 2
-19
-20                else:
-21                    high = mid
-22
-23            ## is there really a condition fo us to use both/ check the left of the right       
-24            else:
-25                if nums[mid] == nums[mid - 1]:
-26                    ## the single element is further right
-27                    low = mid + 1
+14        if nums[l] != nums[l + 1]:
+15            return nums[l]
+16
+17        if nums[r] != nums[r - 1]:
+18            return nums[r]
+19        
+20        # ## neetcode help
+21        # l = 0
+22        # r = len(nums) - 1
+23
+24        # if len(nums) == 1:
+25        #     return nums[0]
+26
+27        # while l <= r:
 28
-29                else:
-30                    high = mid
-31
-32
-33            ## we want to know which way to go look, left or right
-34
-35        return nums[low]
-36        
-37        """
-38        dry run
-39        [1,1,2,3,3,4,4,8,8] 
-40        low = 0
-41        high = 8
-42        mid = 4
-43
-44        4 is even
+29        #     m = (l + r) // 2
+30
+31        #     # while m is in bounds
+32        #     if m - 1 >= 0 and m + 1 < len(nums):
+33        #         if nums[m - 1] != nums[m] and  nums[m + 1] != nums[m]:
+34        #             return nums[m]
+35
+36        #     if nums[m-1] == nums[m]:
+37        #         leftSide = m - 1
+38
+39        #     else:
+40        #         leftSide = m 
+41
+42
+43        #     if leftSide % 2:
+44        #         r = m - 1
 45
-46        [1,1,3,3,4,4,8,8, 9]
-47
-48
+46
+47        #     else:
+48        #         l = m + 1
 49
 50
-51        """
