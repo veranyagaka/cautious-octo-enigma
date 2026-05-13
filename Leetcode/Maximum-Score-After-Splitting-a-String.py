9393e0e6959bc79622dad02b12307ffdc2fa2bae
vera nyagaka <nyagakavera@gmail.com>
2026-05-13 19:20:26 +0300

    [1422] [Time Beats: 100.00%] [Memory Beats: 88.11%] - LeetPush

--- Leetcode/Maximum-Score-After-Splitting-a-String.py
+++ Leetcode/Maximum-Score-After-Splitting-a-String.py
@@ -1,17 +1,22 @@
 1class Solution:
 2    def maxScore(self, s: str) -> int:
 3        
-4        ## brute force
+4        ## optimal use prefix sum
 5
 6        max_score = 0
-7
-8        for i in range(1, len(s)):
-9            left = s[:i]
-10            right = s[i:]
-11
-12            score = left.count("0") + right.count("1")
+7        ones_so_far = 0
+8        total_ones = s.count("1")
+9
+10        for i in range(len(s) - 1):
+11            if s[i] == '1':
+12                ones_so_far += 1
 13
-14            max_score = max(max_score, score)
-15
+14            left_zeros = (i + 1) - ones_so_far
+15            right_ones = total_ones - ones_so_far
 16
-17        return max_score
+17            score = left_zeros + right_ones
+18
+19            max_score = max(max_score, score)
+20
+21
+22        return max_score
