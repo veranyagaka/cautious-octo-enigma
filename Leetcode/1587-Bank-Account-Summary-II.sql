6a41c5a0373b5a660cd17a402582106bc78ba5f7
vera nyagaka <nyagakavera@gmail.com>
2026-09-01 17:58:33 +0300

    [1587] [Time Beats: 77.63%] - LeetPush

---
+++ Leetcode/1587-Bank-Account-Summary-II.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select name, sum(amount) as balance
+from Users u
+inner join Transactions t
+on u.account = t.account
+group by u.account
+having sum(amount) > 10000;
